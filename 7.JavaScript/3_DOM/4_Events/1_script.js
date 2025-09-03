3affc4581430fe245e12d3b7a8219c9cb2240f38
kajalti29 <kajaltisirame@gmail.com>
2025-09-04 00:26:40 +0530

    event commit

---
+++ 7.JavaScript/3_DOM/4_Events/1_script.js
@@ -0,0 +1,26 @@
+let p = document.querySelector("p");
+p.addEventListener("click", function () {
+    p.style.color = "green";
+});
+
+
+let h2 = document.querySelector("h2");
+h2.addEventListener("dblclick", function(){
+    h2.style.color = "yellow";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
