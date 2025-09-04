ff5fb26b9e7e9d81516c59e8f6b0686eb0354779
kajalti29 <kajaltisirame@gmail.com>
2025-09-04 23:09:55 +0530

    pass commit

---
+++ 7.JavaScript/3_DOM/5_FormValidation/1_.js
@@ -0,0 +1,10 @@
+ let nm = document.querySelector("#name");
+let form = document.querySelector("form");
+
+form.addEventListener('submit', function(dets){
+    dets.preventDefault();
+
+   if(nm.ariaValueMax.length <= 2){
+    
+   }
+})
