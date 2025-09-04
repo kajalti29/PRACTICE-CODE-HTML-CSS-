e6f1a294280b9b7db60ddb1d2c4ca66a5606bfae
kajalti29 <kajaltisirame@gmail.com>
2025-09-04 08:30:26 +0530

    new commit

---
+++ 7.JavaScript/3_DOM/4_Events/3_common Events.js
@@ -0,0 +1,11 @@
+// let p = document.querySelector("p");
+// p.addEventListener("click", function(){
+//     p.style.color = "Pink"
+// });
+
+let input = document.querySelector("input");
+
+input.addEventListener("input", function(){
+    console.log("typed");
+    
+})
