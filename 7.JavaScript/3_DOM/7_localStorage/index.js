a8b9288d94ea8c48e2ee67d28f1d675c30d5615c
kajalti29 <kajaltisirame@gmail.com>
2025-09-05 12:47:51 +0530

    new commit

---
+++ 7.JavaScript/3_DOM/7_localStorage/index.js
@@ -0,0 +1,36 @@
+// localStorage :-apke browser ke ander data store karna jo ki brower band hone pr bhi delete nhi hoga
+// SessionStorage:- Ye apkadata temporarily store karta hai matlab ki tab band hua aur data gaya
+
+//Cookies :-ye bhi data store karta hai and aapka data broer ke cookies naam ki property mein save hota hai . ye chote data ko store karte hai.
+
+//Abhi padna hai:-
+
+// localStorage
+// store kaise Kere  -  seItem
+// data fetch kaise Kere  - getItem
+// remove kaise kare 
+// update kaise kare 
+
+
+
+
+localStorage.setItem("name", "kajalti");
+localStorage.getItem("name", "kajalti");
+
+localStorage.removeItemItem("name");
+localStorage.setItem("name", "kajalti");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
