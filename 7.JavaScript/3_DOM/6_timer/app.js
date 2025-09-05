4e5acbcc9013080c764d4782e9a06bc94b75a502
kajalti29 <kajaltisirame@gmail.com>
2025-09-05 11:02:54 +0530

    new commit

---
+++ 7.JavaScript/3_DOM/6_timer/app.js
@@ -0,0 +1,9 @@
+// setTimeout( function(){
+//     console.log("Hello");
+    
+// }, 5000)
+
+setInterval( function(){
+    console.log("Hello");
+    
+}, 5000)
---
+++ 7.JavaScript/3_DOM/6_timer/index.js
@@ -0,0 +1,7 @@
+let count = 10;
+let interval = setInterval(function (){
+    if(count >= 1){
+        count--;
+        console.log(count);   
+    }else clearInterval(interval);
+ }, 1000);
