10a2344366317a5954fd2477dab93bb9d4cbed6e
kajalti@29 <kajaltisirame@gmail.com>
2025-11-19 13:54:20 +0530

    functioon commit

---
+++ 11_javaS/01_Practice/02_Data types/8_Object.js
@@ -0,0 +1,7 @@
+let student = {
+    name: "Kajalti",
+    age: 21,
+    isEnrolled: true,
+    courses: ["JavaScript", "HTML", "CSS"],
+}
+console.log(student);
---
+++ 11_javaS/01_Practice/02_Data types/9_Array.js
@@ -0,0 +1,2 @@
+let marks = [90, 85, 70];
+console.log(marks);
