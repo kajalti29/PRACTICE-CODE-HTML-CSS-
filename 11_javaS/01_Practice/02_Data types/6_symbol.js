366013850331106a44b5dd486ebef21e10687ad5
kajalti@29 <kajaltisirame@gmail.com>
2025-11-19 13:46:23 +0530

    code commit

--- 11_javaS/01_Practice/02_Data types/6_symbol.js
+++ 11_javaS/01_Practice/02_Data types/6_symbol.js
@@ -1,7 +1,3 @@
-//   represent a unique identifier. Symbols are immutable and are often used to create unique property keys for objects to avoid name collisions.
-let sym1 = Symbol('description1');
-let sym2 = Symbol('description2');
-
-console.log(sym1); // Output: Symbol(description1)
-console.log(sym2); // Output: Symbol(description2)
-console.log(sym1 === sym2); // Output: false, because each Symbol is unique
+// Unique value create karta hai
+let id = Symbol("123");
+console.log(id);
