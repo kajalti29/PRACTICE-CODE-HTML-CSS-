57a4c26a84284ff0b35d21aa87457f3fe7052408
kajalti@29 <kajaltisirame@gmail.com>
2025-11-20 22:02:45 +0530

    code commit

---
+++ 7.JavaScript/01_.Js codes/Practice function/01_fun.js
@@ -0,0 +1,18 @@
+// function hello(){
+//     console.log("Hello World");
+// }
+// hello();
+
+
+// let greet = function(){
+//     console.log("Good Morning");
+// }
+// greet();
+
+// let add = (a, b) => a + b;
+// console.log(add(3,2));
+
+function show() {
+  console.log("Running...");
+}
+show();
